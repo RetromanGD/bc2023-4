a2418b698dab3ef33215fc12999b1c3e506859f2
RetromanGD <bodyaITshnik@ukr.net>
2023-10-29 15:49:25 +0200

    second commit

--- index.js
+++ index.js
@@ -1,30 +1,33 @@
 const http = require('http');
 const fs = require('fs');
 const { XMLParser, XMLBuilder } = require("fast-xml-parser");
-// Створення веб-сервера
+
 const server = http.createServer((req, res) => {
-    // Зчитування XML-документу з файлу 'data.xml'
     fs.readFile('data.xml', 'utf8', (err, data) => {
-        if (err) {
-            res.writeHead(500, { 'Content-Type': 'text/plain' });
-            res.end('Internal Server Error');
-            return;
-        }
-        // Створення екземпляра парсера для розбору XML
-        const parser = new XMLParser();
-        const parsedData = parser.parse(data);
-        let maxRate = 0;
+        if (err===null){
+        const parsexml = new XMLParser();
+
+        const parsedData = parsexml.parse(data);
+
+        let maxR = 0;
         parsedData.exchange.currency.forEach(currency => {
-            if (parseFloat(currency.rate) > maxRate) {
-                maxRate = parseFloat(currency.rate);
+            if (parseFloat(currency.rate) > maxR) {
+                maxR = parseFloat(currency.rate);
             }
         });
-        const builder = new XMLBuilder();
-        const xmlContent = builder.build({ data: { max_rate: maxRate.toString() } });
+
+        const buildxml = new XMLBuilder();
+
+        const xmlForSite = buildxml.build({ data: { max_rate: maxR.toString() } });
 
         res.writeHead(200, { 'Content-Type': 'application/xml' });
-        res.write(xmlContent);
+        res.write(xmlForSite);
         res.end();
+        } else {
+            res.writeHead(500, { 'Content-Type': 'text/plain' });
+            res.end('Error');
+            return;
+        }
     });
 });
 
